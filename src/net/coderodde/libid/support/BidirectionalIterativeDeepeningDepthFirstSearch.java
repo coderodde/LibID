ee80862ea6160e36926a68c30e31255c21c5aa2b
coderodde <rodion.efremov@cs.helsinki.fi>
2017-05-01 16:12:57 +0300

    Filled out the bidirectional IDDFS. Next debugging.

--- src/net/coderodde/libid/support/BidirectionalIterativeDeepeningDepthFirstSearch.java
+++ src/net/coderodde/libid/support/BidirectionalIterativeDeepeningDepthFirstSearch.java
@@ -1,8 +1,128 @@
 package net.coderodde.libid.support;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.HashSet;
 import java.util.List;
-import net.coderodde.libid.SlidingTilePuzzleNode;
+import java.util.Set;
+import net.coderodde.libid.NodeExpander;
 
-public final class BidirectionalIterativeDeepeningDepthFirstSearch {
+public final class BidirectionalIterativeDeepeningDepthFirstSearch<N> {
 
+    private final List<N> forwardSearchStack;
+    private final List<N> backwardSearchStack;
+    private final Set<N> frontier;
+    private final NodeExpander<N> forwardExpander;
+    private final NodeExpander<N> backwardExpander;
+    private boolean found = false;
+    private int reachedNodesInForwardSearch          = 0;
+    private int previousReachedNodesInForwardSearch  = 0;
+    private int reachedNodesInBackwardSearch         = 0;
+    private int previousReachedNodesInBackwardSearch = 0;
+    
+    public BidirectionalIterativeDeepeningDepthFirstSearch() {
+        this.forwardSearchStack = null;
+        this.backwardSearchStack = null;
+        this.frontier = null;
+        this.forwardExpander = null;
+        this.backwardExpander = null;
+    }
+    
+    private BidirectionalIterativeDeepeningDepthFirstSearch(
+        NodeExpander<N> forwardExpander,
+        NodeExpander<N> backwardExpander) {
+        this.forwardSearchStack = new ArrayList<>();
+        this.backwardSearchStack = new ArrayList<>();
+        this.frontier = new HashSet<>();
+        this.forwardExpander = forwardExpander;
+        this.backwardExpander = backwardExpander;
+    }
+    
+    public List<N> search(N source, 
+                          N target, 
+                          NodeExpander<N> forwardExpander,
+                          NodeExpander<N> backwardExpander) {
+        BidirectionalIterativeDeepeningDepthFirstSearch<N> state = 
+                new BidirectionalIterativeDeepeningDepthFirstSearch<>(
+                        forwardExpander,
+                        backwardExpander);
+        
+        for (int depth = 0;; ++depth) {
+            // Do a depth limited search in forward direction. Put all nodes at 
+            // depth == 0 to the frontier.
+            state.depthLimitedSearchForward(source, depth);
+            
+            if (state.previousReachedNodesInForwardSearch == 
+                    state.reachedNodesInForwardSearch) {
+                throw new IllegalStateException("target not reachable");
+            }
+            
+            // Perform a reversed search starting from the target node and 
+            // recurring to the depth 'depth'.
+            state.depthLimitedSearchBackward(target, depth);
+            
+            if (state.found) {
+                return buildPath();
+            }
+            
+            if (state.previousReachedNodesInBackwardSearch ==
+                    state.reachedNodesInBackwardSearch) {
+                throw new IllegalStateException("target not reachable");
+            }
+            
+            // Perform a reversed search once again with depth = 'depth + 1'.
+            // We need this in case the shortest path has odd number of arcs.
+            state.depthLimitedSearchBackward(target, depth + 1);
+            
+            if (state.found) {
+                return buildPath();
+            }
+            
+            if (state.previousReachedNodesInBackwardSearch ==
+                    state.reachedNodesInBackwardSearch) {
+                throw new IllegalStateException("target not reachable");
+            }
+            
+            frontier.clear();
+        }
+    }
+    
+    private void depthLimitedSearchForward(N node, int depth) {
+        ++reachedNodesInForwardSearch;
+        forwardSearchStack.add(node);
+        
+        if (depth == 0) {
+            frontier.add(node);
+            return;
+        }
+        
+        for (N child : forwardExpander.expand(node)) {
+            depthLimitedSearchForward(child, depth - 1);
+        }
+    }
+    
+    private void depthLimitedSearchBackward(N node, int depth) {
+        ++reachedNodesInBackwardSearch;
+        backwardSearchStack.add(node);
+        
+        if (depth == 0) {
+            found = frontier.contains(node);
+            return;
+        }
+        
+        for (N parent : backwardExpander.expand(node)) {
+            depthLimitedSearchForward(parent, depth - 1);
+        }
+    }
+    
+    private List<N> buildPath() {
+        List<N> path = new ArrayList<>(forwardSearchStack.size() + 
+                                       backwardSearchStack.size());
+        Collections.<N>reverse(backwardSearchStack);
+        path.addAll(forwardSearchStack);
+        path.addAll(backwardSearchStack);
+        return path;
+    }
 }

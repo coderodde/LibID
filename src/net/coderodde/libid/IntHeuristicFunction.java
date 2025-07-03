efe9ce148a79307fc57fd787b4e9f7312e0a03bb
Rodion Efremov <coderodd3@yandex.com>
2025-07-03 18:15:31 +0300

    Removed the false directory net and friends.

--- src/net/coderodde/libid/IntHeuristicFunction.java
+++
@@ -1,6 +0,0 @@
-package net.coderodde.libid;
-
-public interface IntHeuristicFunction<N> {
-
-    public int estimate(N current, N target);
-}
--- src/net/coderodde/libid/NodeExpander.java
+++
@@ -1,8 +0,0 @@
-package net.coderodde.libid;
-
-import java.util.Collection;
-
-public interface NodeExpander<N> {
-
-    public Collection<N> expand(N node);
-}
